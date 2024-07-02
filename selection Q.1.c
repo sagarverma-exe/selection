a6bb0cc1c222c57d8bbc4fff40c6fbec6dbe4cb6
Sagar Verma <8737sagar@gmail.com>
2024-07-02 15:48:51 +0900

    Add files via upload

---
+++ selection Q.1.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main(){
+
+   int X,Y,Z;
+printf("enter the X number : ");
+ scanf("%d",&X);
+
+printf("enter the Y number : ");
+ scanf("%d",&Y);
+
+printf("enter the Z number : ");
+ scanf("%d",&Z);
+
+(X<Y)?(X<Z)?printf("X is minimum"):printf("Z is minimum"):
+
+(Y<Z)? 
+     printf("Y is minimum")
+		:printf("Z is minimum");
+
+
+
+
+}
---
+++ switchQ.2.c
@@ -0,0 +1,59 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+int choice ;
+printf("Airtel call center\n");
+
+printf("press 1 for english\n");
+printf("press 2 for hindi\n");
+printf("press 3 for gujrati\n");
+
+printf("enter your choice:=");
+scanf("%d",&choice);
+
+
+switch(choice)
+{
+
+case 1:printf("press 1 for Internet Recharge");
+
+	   printf("press 2 for Top-up Recharge");
+
+	   printf("press 3 for Special Recharge");
+
+
+default:printf("Enter vaild input");
+
+switch (choice)
+{
+
+case 2:printf("You have successfully done your Internet Recharge");
+       printf("You have successfully done your Top-up Recharge");
+       printf("You have successfully done your Special Recharge");
+break;
+
+default:printf("Enter vaild input");
+}
+
+{
+
+switch(choice)
+
+break;
+case 3:printf("Aapka safaltapurvak Internet Recharge karliya he");
+
+       printf("Aapka safaltapurvak Top-up Recharge karliya he");
+
+       printf("Aapka safaltapurvak Special Recharge karliya he");
+
+break;
+default:printf("Enter valid Input");
+}
+
+
+
+
+
+}
